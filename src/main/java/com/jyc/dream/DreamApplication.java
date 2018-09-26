9d158b5d04943be664681b5fda4fa1f530bcb7cf
jiayc <jiayc@visionet.com.cn>
2018-09-26 15:15:10 +0800

    提交

--- src/main/java/com/jyc/dream/DreamApplication.java
+++ src/main/java/com/jyc/dream/DreamApplication.java
@@ -3,10 +3,17 @@ package com.jyc.dream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author jyc
+ * 启动类
+ *
+ */
 @SpringBootApplication
 public class DreamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DreamApplication.class, args);
     }
+
+
 }
